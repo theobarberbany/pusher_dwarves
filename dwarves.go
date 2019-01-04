3d7255f0f0d88231e987eb02b8a33691c6a8325c
Theo Barber-Bany <theobarberbany@gmail.com>
2019-01-04 00:07:49 +0000

    Unmarshal to JSON

--- dwarves.go
+++ dwarves.go
@@ -4,24 +4,29 @@ import (
 	"fmt"
 	"io/ioutil"
 	//"log"
+	"encoding/json"
 	"net/http"
 	"time"
 )
 
 type Dwarf struct {
-	Name    string
-	Birth   string
-	Death   string
-	Culture string
+	Name    string `json:"name"`
+	Birth   string `json:"birth"`
+	Death   string `json:"death"`
+	Culture string `json:"culture"`
+}
+
+type DwarfData struct {
+	Dwarves []Dwarf `json:"dwarves"`
 }
 
 // getDwarves wraps a GET request in a retry function. This is because the dwarves are
 // sometimes busy. Url specifies where to find the dwarves, and retries how many times to
 // retry before giving up.
-// The body of the get response is returned as a string.
+// The body of the get response is returned as a slice of bytes.
 // There is a back off between retries.
-func getDwarves(url string, retries int) (string, error) {
-	// build get request and response outside of retry func
+func getDwarves(url string, retries int) (*[]byte, error) {
+	// Build get request and response outside of retry func
 	resp := &http.Response{}
 	req, err := http.NewRequest(
 		"GET",
@@ -29,7 +34,7 @@ func getDwarves(url string, retries int) (string, error) {
 		nil)
 
 	if err != nil {
-		return "", fmt.Errorf("unable to create request", err)
+		return nil, fmt.Errorf("unable to create request", err)
 	}
 
 	err_retry := retry(retries, time.Second, func() error {
@@ -51,11 +56,19 @@ func getDwarves(url string, retries int) (string, error) {
 	})
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	return string(body), err_retry
+	return &body, err_retry
 }
 
 func main() {
 	// Get JSON information on the dwarves.
-	body, _ := getDwarves("https://thedwarves.pusherplatform.io/api/dwarves", 5)
-	fmt.Printf("Body: %s", body)
+	dwarfJson, _ := getDwarves("https://thedwarves.pusherplatform.io/api/dwarves", 5)
+	fmt.Printf("Body: %s\n\n\n", string(*dwarfJson))
+
+	var dwarves DwarfData
+	err := json.Unmarshal(*dwarfJson, &dwarves)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(dwarves)
+
 }
